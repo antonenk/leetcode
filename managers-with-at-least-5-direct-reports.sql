089c93508422d5abb72bd6c84f46cb47b8f3d784
Leonid Antonenkov <antonenk@intermedia.com>
2024-10-22 19:21:26 +0300

    new format for .sql with example data

--- managers-with-at-least-5-direct-reports.sql
+++ managers-with-at-least-5-direct-reports.sql
@@ -1,7 +1,24 @@
+-- https://leetcode.com/problems/managers-with-at-least-5-direct-reports/description/
+
+CREATE TEMPORARY TABLE Employee (
+    id int,
+    name varchar(255),
+    department varchar(255),
+    managerId int,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO Employee (id, name, department, managerId) VALUES
+    ('101', 'John', 'A', NULL),
+    ('102', 'Dan', 'A', '101'),
+    ('103', 'James', 'A', '101'),
+    ('104', 'Amy', 'A', '101'),
+    ('105', 'Anne', 'A', '101'),
+    ('106', 'Ron', 'B', '101');
+
 SELECT
-    e.name
+    name
 FROM
-    (SELECT managerId AS id FROM Employee WHERE managerId IS NOT NULL GROUP BY managerId HAVING COUNT(*)>=5) AS m
-    LEFT JOIN Employee e ON e.id=m.id
+    Employee
 WHERE
-    e.id IS NOT NULL
+    id IN (SELECT managerId AS id FROM Employee GROUP BY managerId HAVING COUNT(*)>=5)
