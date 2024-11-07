29cb60ce39d7e3d93cbd63cbd8a0c10718582acd
Leonid Antonenkov <antonenk@intermedia.com>
2024-11-07 16:57:44 +0300

    beats 100% runtime / 84% mem

---
+++ concatenation-of-array.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# https://leetcode.com/problems//description/
+
+
+class Solution:
+    def getConcatenation(self, nums: list[int]) -> list[int]:
+        nums.extend(nums)
+        return nums
+
+
+def test_1():
+    assert Solution().getConcatenation([1, 2, 1]) == [1, 2, 1, 1, 2, 1]
+
+
+def test_2():
+    assert Solution().getConcatenation([1, 3, 2, 1]) == [1, 3, 2, 1, 1, 3, 2, 1]
+
+
+if __name__ == '__main__':
+    test_1()
+    test_2()
