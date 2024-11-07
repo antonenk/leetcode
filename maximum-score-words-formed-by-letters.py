99d9746e4e0b596193c04fcf2708f56c7654b1bb
Leonid Antonenkov <antonenk@intermedia.com>
2024-11-07 14:52:05 +0300

    no solution, just tests

---
+++ maximum-score-words-formed-by-letters.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+# https://leetcode.com/problems/maximum-score-words-formed-by-letters/description/
+
+
+class Solution:
+    def maxScoreWords(self, words: list[str], letters: list[str], score: list[int]) -> int:
+        return 23
+
+
+def test_1():
+    words = ["dog", "cat", "dad", "good"]
+    letters = ["a", "a", "c", "d", "d", "d", "g", "o", "o"]
+    score = [1, 0, 9, 5, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    assert Solution().maxScoreWords(words, letters, score) == 23
+
+
+def test_2():
+    words = ["xxxz", "ax", "bx", "cx"]
+    letters = ["z", "a", "b", "c", "x", "x", "x"]
+    score = [4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 10]
+    assert Solution().maxScoreWords(words, letters, score) == 27
+
+
+def test_3():
+    words = ["leetcode"]
+    letters = ["l", "e", "t", "c", "o", "d"]
+    score = [0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
+    assert Solution().maxScoreWords(words, letters, score) == 0
+
+
+if __name__ == '__main__':
+    test_1()
+    test_2()
+    test_3()
