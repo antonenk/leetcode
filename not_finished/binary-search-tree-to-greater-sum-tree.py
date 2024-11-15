5cc91b629e52d6363d94ad8d677c098dadbc04db
Leonid Antonenkov <antonenk@intermedia.com>
2024-11-15 13:33:00 +0300

    store not finished separately

---
+++ not_finished/binary-search-tree-to-greater-sum-tree.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+# https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/description/
+# beats % runtime / % mem
+
+
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def addGreaterValuesToNodes(node: TreeNode, sumGreaterNodes):
+    if node.right is not None:
+        sumGreaterNodes += addGreaterValuesToNodes(node.right, sumGreaterNodes)
+    valBeforeIncrease = node.val
+    node.val += sumGreaterNodes
+    print(f"{valBeforeIncrease} --> {node.val}")
+    sumGreaterNodes += valBeforeIncrease
+    if node.left is not None:
+        sumGreaterNodes = addGreaterValuesToNodes(node.left, sumGreaterNodes)
+    return sumGreaterNodes
+
+
+class Solution:
+    def bstToGst(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        addGreaterValuesToNodes(root, 0)
+        return root
+
+
+def test_1():
+    root = TreeNode(
+        val=4,
+        left=TreeNode(
+            val=1,
+            left=TreeNode(val=0),
+            right=TreeNode(
+                val=2,
+                right=TreeNode(val=3)
+                )
+            ),
+        right=TreeNode(
+            val=6,
+            left=TreeNode(val=5),
+            right=TreeNode(
+                val=7,
+                right=TreeNode(val=8)
+                )
+            )
+        )
+    Solution().bstToGst(root)
+    assert 1 == 1
+
+
+if __name__ == '__main__':
+    test_1()
