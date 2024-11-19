4893491c4509e0208df53894a24ceb510023cdcc
Leonid Antonenkov <antonenk@intermedia.com>
2024-11-19 15:58:17 +0300

    beats 100% runtime / 36% mem

--- binary-search-tree-to-greater-sum-tree.py
+++ binary-search-tree-to-greater-sum-tree.py
@@ -5,7 +5,13 @@
 
 
 from typing import Optional
-from leetcode import TreeNode
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
 def addGreaterValuesToNodes(node: TreeNode, sumGreaterNodes):
