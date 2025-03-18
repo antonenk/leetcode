3ea9dfd770f05749fd9e14265e80fee62f33a8b5
Leonid Antonenkov <antonenk@intermedia.com>
2025-03-18 12:18:58 +0300

    beats 12% runtime / 98% mem

--- letter-case-permutation.py
+++ letter-case-permutation.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3
 
 # https://leetcode.com/problems/letter-case-permutation/description/
-# beats 9% runtime / 74% mem
+# beats 12% runtime / 98% mem
 
 
 class Solution:
     def letterCasePermutation(self, s: str) -> list[str]:
         template = list(s)
         digits = set(list("0123456789"))
-        letterPositions = []
+        letters = []
         for position in range(len(template)):
-            if template[position] not in digits:
-                letterPositions.append(position)
+            symbol = template[position]
+            if symbol not in digits:
+                letters.append((position, symbol.lower(), symbol.upper()))
 
-        lettersCount = len(letterPositions)
+        lettersCount = len(letters)
         maskFormat = '0' + str(lettersCount) + 'b'
         result = []
         for mask in range(2 ** lettersCount):
             maskString = format(mask, maskFormat)
             for letterId in range(lettersCount):
-                letterPosition = letterPositions[letterId]
-                letter = template[letterPosition]
+                position, lower, upper = letters[letterId]
                 if maskString[letterId] == '0':
-                    template[letterPosition] = letter.lower()
+                    template[position] = lower
                 else:
-                    template[letterPosition] = letter.upper()
+                    template[position] = upper
             result.append("".join(template))
 
         return result
