82ec393f77b3b6950bc366217361aba3c52d4463
Leonid Antonenkov <antonenk@intermedia.com>
2025-04-10 13:01:07 +0300

    beats 59% runtime / 99% mem

--- all-possible-full-binary-trees.py
+++ all-possible-full-binary-trees.py
@@ -1,30 +1,33 @@
 #!/usr/bin/python3
 
 # https://leetcode.com/problems/all-possible-full-binary-trees/description/
-# beats 5% runtime / 5% mem
+# beats 59% runtime / 99% mem
 
 from leetcode import TreeNode
 
 
-def allFBT(n: int) -> list[TreeNode]:
-    if n == 1:
-        return [TreeNode(val=0)]
-    elif n == 3:
-        return [TreeNode(val=0, left=TreeNode(val=0), right=TreeNode(val=0))]
-    else:
-        trees = []
-        for countLeft in range(1, n, 2):
-            leftTrees = allFBT(countLeft)
-            for leftTree in leftTrees:
-                rightTrees = allFBT(n - countLeft - 1)
-                for rightTree in rightTrees:
-                    trees.append(TreeNode(val=0, left=leftTree, right=rightTree))
-        return trees
-
-
 class Solution:
     def allPossibleFBT(self, n: int) -> list[TreeNode]:
-        return allFBT(n)
+        if n % 2 == 0:
+            return []
+        fbts: list[list[TreeNode]] = [
+            [],
+            [TreeNode(val=0)],
+            [],
+            [TreeNode(val=0, left=TreeNode(val=0), right=TreeNode(val=0))]
+        ]
+        if n < 5:
+            return fbts[n]
+        for nodesCount in range(5, n + 1, 2):
+            trees = []
+            for countLeft in range(1, nodesCount, 2):
+                for leftTree in fbts[countLeft]:
+                    for rightTree in fbts[nodesCount - countLeft - 1]:
+                        trees.append(TreeNode(val=0, left=leftTree, right=rightTree))
+            fbts.append([])
+            fbts.append(trees)
+
+        return fbts[n]
 
 
 def test_1():
@@ -42,7 +45,13 @@ def test_3():
     assert len(output) == 2
 
 
+def test_4():
+    output = Solution().allPossibleFBT(100)
+    assert len(output) == 0
+
+
 if __name__ == '__main__':
     test_1()
     test_2()
     test_3()
+    test_4()
